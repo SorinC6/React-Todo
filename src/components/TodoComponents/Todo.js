f80a5feec731994b7ba1402e5fb779ed83fb03e9
Sorin Chis <kish.sorin@yahoo.com>
2019-02-13 16:40:38 +0200

    changing state: completed true  when you click on todos

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Todo = (props) => {
    return <div
       // className={props.todos.completed ? 'completed' : ''} .........??
-      onClick={() => props.onToggle(props.todoItem.id)} 
+      onClick={props.onToggle}
       >
       
       {props.todoItem}</div>;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -11,7 +11,7 @@ const TodoList = (props) => {
                <Todo 
                   key={todo.id} 
                   todoItem={todo.task}
-                  onToggle={props.onToggle}    
+                  onToggle={() => props.onToggle(todo.id)}     
                />
             );
 			})}
