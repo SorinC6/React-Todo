dd43ba386558973a237fe0c317f5c041f1c5c663
Sorin Chis <kish.sorin@yahoo.com>
2019-02-13 14:00:51 +0200

    added TodoList component and setting the state on App.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,12 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'React';
+import Todo from './Todo'
+
+const TodoList = (props) => {
+   return(
+      {
+         props.todoData.map(todo => <Todo todoItem={todo.task}/>),
+      }
+   )
+}
